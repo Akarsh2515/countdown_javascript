4f31a8759ad8db6cc94b9390c293297d53f5d76e
Akarsh2515 <69716162+Akarsh2515@users.noreply.github.com>
2023-06-11 03:05:48 +0530

    Add files via upload

---
+++ countdownproject js/app.js
@@ -0,0 +1,22 @@
+const endDate="12 june 2023 02:45 PM"
+document.getElementById("end-date").innertext=endDate;
+const inputs=document.querySelectorAll("input")
+
+function clock()
+{
+    const end=new Date(endDate);
+    const now=new Date();
+    const diff=(end-now)/1000;
+    console.log(diff);
+    inputs[0].value=Math.floor(diff/3600/24);
+    inputs[1].value=Math.floor(diff/3600)%24;
+    inputs[2].value=Math.floor(diff/60)%60;
+    inputs[3].value=Math.floor(diff)%60;
+}
+clock()
+
+setInterval(
+    ()=>{
+        clock()
+    },1000
+)
